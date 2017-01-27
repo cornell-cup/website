95c0ce4830ab439ea92f677c9688c9fabbddb86d
Scott Wu <scott.wu.hjx@gmail.com>
2017-01-26 23:48:30 -0500

    Add minimum website build

--- build.js
+++ build.js
@@ -1,3 +1,74 @@
+var async = require("async");
 var fs = require("fs");
 var glob = require("glob");
 var Mustache = require("mustache");
+var ncp = require("ncp");
+var path = require("path");
+
+// Create the output directory
+try {
+    if (!fs.lstatSync("build").isDirectory()) {
+        throw new Error("'./build' is not a directory");
+    }
+}
+catch (e) {
+    fs.mkdirSync("build");
+}
+
+// Read the raw special pages
+var headerRaw = fs.readFileSync("src/header.html").toString();
+var footerRaw = fs.readFileSync("src/footer.html").toString();
+var redirectRaw = fs.readFileSync("src/redirect.html").toString();
+
+// Find all pages
+var pagePaths = glob.sync("src/pages/*");
+var fileNames = pagePaths.map(function(page) {
+    return path.basename(page, ".html");
+});
+pagePaths.forEach(function(pagePath) {
+    // Read the raw page
+    var pageName = path.basename(pagePath, ".html");
+    var pageRaw = fs.readFileSync(pagePath).toString();
+
+    // Compute data view
+    var data = {
+        header: "",
+        footer: "",
+        pages: fileNames.map(fileName => ({
+            name: fileName,
+            active: fileName === pageName
+        }))
+    };
+
+    // Render the header and footer first
+    var header = Mustache.render(headerRaw, data);
+    var footer = Mustache.render(footerRaw, data);
+
+    // Add to the data view
+    data.header = header;
+    data.footer = footer;
+
+    // Render the page
+    var page = Mustache.render(pageRaw, data);
+
+    // Write to the output directory
+    fs.writeFileSync("build/" + pageName + ".html", page);
+
+    // Also write a redirect directory (/home => /home.html)
+    try {
+        fs.mkdirSync("build/" + pageName);
+    }
+    catch (e) {
+    }
+    fs.writeFileSync("build/" + pageName + "/index.html", redirectRaw);
+});
+
+
+// Copy resource folders
+async.map(["extra/*", "css", "js", "images"],
+    function(folder, callback) {
+        ncp.ncp("src/" + folder, "build/" + folder, function(err) {
+            callback(err);
+        });
+    }
+);
